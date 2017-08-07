82b58e59ef7f95fbd1521d2642b81289138954d0
Pyrolistical <pyrogx1133@gmail.com>
2017-08-06 23:33:03 -0700

    parse null and booleans

--- parser.js
+++ parser.js
@@ -1,91 +1,94 @@
-const {ParserFactory, oneOf, optional, manyOf, separated, Node} = require('./parser-factory')
+const {ParserFactory, oneOf, optional, ref} = require('./parser-factory')
 
 module.exports = ParserFactory({
   Null: 'null',
 
   Boolean: oneOf('true', 'false'),
 
-  Number: [
-    optional('-'),
-    manyOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'),
-    optional([
-      '.',
-      manyOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    ]),
-    optional([
-      oneOf('e', 'E'),
-      oneOf('-', '+'),
-      manyOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    ])
-  ],
+  // Number: [
+  //   optional('-'),
+  //   manyOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'),
+  //   optional([
+  //     '.',
+  //     manyOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+  //   ]),
+  //   optional([
+  //     oneOf('e', 'E'),
+  //     oneOf('-', '+'),
+  //     manyOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+  //   ])
+  // ],
 
-  String: oneOf(
-    [
-      '"',
-      /a-zA-Z/,
-      '"',
-    ],
-    [
-      '\'',
-      /a-zA-Z/,
-      '\''
-    ]
-  ),
+  // String: oneOf(
+  //   [
+  //     '"',
+  //     /a-zA-Z/,
+  //     '"',
+  //   ],
+  //   [
+  //     '\'',
+  //     /a-zA-Z/,
+  //     '\''
+  //   ]
+  // ),
 
-  Whitespace: manyOf(
-    ' ',
-    '\t'
-  ),
+  // Whitespace: manyOf(
+  //   ' ',
+  //   '\t',
+  //   '\r\n',
+  //   '\r',
+  //   '\n'
+  // ),
 
-  Array: [
-    '[',
-    optional(
-      optional(Node('Whitespace')),
-      separated(Node('Expression'), [
-        optional(Node('Whitespace')),
-        ',',
-        optional(Node('Whitespace'))
-      ]),
-      optional(Node('Whitespace'))
-    ),
-    ']'
-  ],
+  // Array: [
+  //   '[',
+  //   optional(
+  //     optional(ref('Whitespace')),
+  //     separated(ref('Expression'), [
+  //       optional(ref('Whitespace')),
+  //       ',',
+  //       optional(ref('Whitespace'))
+  //     ]),
+  //     optional(ref('Whitespace'))
+  //   ),
+  //   ']'
+  // ],
 
-  Object: [
-    '{',
-    optional(
-      optional(Node('Whitespace')),
-      separated(Node('Property'), [
-        optional(Node('Whitespace')),
-        ',',
-        optional(Node('Whitespace'))
-      ]),
-      optional(Node('Whitespace'))
-    ),
-    '}'
-  ],
+  // Object: [
+  //   '{',
+  //   optional(
+  //     optional(ref('Whitespace')),
+  //     separated(ref('Property'), [
+  //       optional(ref('Whitespace')),
+  //       ',',
+  //       optional(ref('Whitespace'))
+  //     ]),
+  //     optional(ref('Whitespace'))
+  //   ),
+  //   '}'
+  // ],
 
-  Property: [
-    Node('String'),
-    optional(Node('Whitespace')),
-    ':',
-    optional(Node('Whitespace')),
-    Node('Expression')
-  ],
+  // Property: [
+  //   ref('String'),
+  //   optional(ref('Whitespace')),
+  //   ':',
+  //   optional(ref('Whitespace')),
+  //   ref('Expression')
+  // ],git 
 
   Expression: oneOf(
-    Node('Null'),
-    Node('Boolean'),
-    Node('Number'),
-    Node('String'),
-    Node('Array'),
-    Node('Object'),
-    [
-      '(',
-      Node('Expression'),
-      ')'
-    ]
+    ref('Null'),
+    ref('Boolean')
+    // ref('Number'),
+    // ref('String'),
+    // ref('Array'),
+    // ref('Object'),
+    // [
+    //   '(',
+    //   ref('Expression'),
+    //   ')'
+    // ]
   ),
 
-  Root: optional(Node('Expression'))
+  Root: optional(ref('Expression'))
 })
--- parser.spec.js
+++ parser.spec.js
@@ -1,5 +1,16 @@
-describe('parser', () => {
-  it('should', () => {
+const parser = require('./parser')
 
+function shouldParse(source) {
+  it(`should parse ${source}`, () => {
+    expect(parser(source).type).toBe('Success')
+  })
+}
+describe('parser', () => {
+  shouldParse('')
+
+  describe('literal', () => {
+    shouldParse('null')
+    shouldParse('true')
+    shouldParse('false')
   })
 })
